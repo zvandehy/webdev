21da6aeae8148ae8d890457884a11fc397f32b5c
zvandehy <evandehy@uncc.edu>
2021-03-09 11:05:29 -0500

    index ejs and partials

---
+++ booksmart/controllers/textbookController.js
@@ -0,0 +1,73 @@
+// const model = require('../models/textbook.js');
+
+
+// GET /stories: send all stories to the user
+exports.index = (req, res) => {
+    // let stories = model.find();
+    // res.render('./story/index', { stories }) //"/views" is implicit
+    res.sendFile("../views/index.html");
+};
+
+// // GET /stories/new: send the form to create a new story
+// exports.new = (req, res) => {
+//     res.render('./story/new');
+// };
+
+// // POST /stories
+// exports.create = (req, res) => {
+//     // save story in DB
+//     let story = req.body;
+//     model.save(story);
+
+//     // simply redirect, will be captured by /stories endpoint above
+//     res.redirect('/stories');
+// };
+
+// // GET /stories/:id
+// exports.show = (req, res, next) => {
+//     let story = model.findById(req.params.id);
+//     if (story) {
+//         res.render("./story/show", { story });
+//     } else {
+//         let err = new Error("Cannot find a story with id " + req.params.id);
+//         err.status = 404;
+//         next(err);
+//     }
+// };
+
+// // GET /stories/:id/edit
+// exports.edit = (req, res, next) => {
+//     let story = model.findById(req.params.id);
+//     if (story) {
+//         res.render("./story/edit", { story });
+//     } else {
+//         let err = new Error("Cannot find a story with id " + req.params.id);
+//         err.status = 404;
+//         next(err);
+//     }
+// };
+
+// // PUT /stories/:id --> Update the story identified by id
+// exports.update = (req, res, next) => {
+//     // update this story in DB
+//     let story = req.body;
+//     let id = req.params.id;
+//     if (model.updateById(id, story)) {
+//         res.redirect('/stories/' + id);
+//     } else {
+//         let err = new Error("Cannot find a story with id " + id);
+//         err.status = 404;
+//         next(err);
+//     }
+// };
+
+// // DELETE /stories/:id: Delete the story identified by id
+// exports.delete = (req, res, next) => {
+//     if (model.deleteById(req.params.id)) {
+//         res.redirect("/stories");
+//     } else {
+//         let err = new Error("Cannot find a story with id " + req.params.id);
+//         err.status = 404;
+//         next(err);
+//     }
+// };
---
+++ booksmart/routes/textbookRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const controller = require("../controllers/textbookController.js");
+const router = express.Router();
+
+// GET /stories: send all stories to the user
+router.get("/", controller.index);
+
+// GET /stories/new: send the form to create a new story
+router.get("/new", controller.new);
+
+// POST /stories
+router.post("/", controller.create);
+
+// GET /stories/:id
+router.get("/:id", controller.show);
+
+// GET /stories/:id/edit
+router.get("/:id/edit", controller.edit);
+
+// PUT /stories/:id: Update the story identified by id
+router.put("/:id", controller.update);
+
+// DELETE /stories/:id: Delete the story identified by id
+router.delete("/:id", controller.delete);
+
+module.exports = router;
